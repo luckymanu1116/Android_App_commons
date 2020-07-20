55599349870d66d6429b98abeb1d27a3af6d1ae0
Manoj Reddy <luckymanoj1116@gmail.com>
2020-07-20 14:30:27 +0530

    Add files via upload

---
+++ bookmarks/locations/BookmarkLocationsFragment.java
@@ -0,0 +1,100 @@
+package fr.free.nrw.commons.bookmarks.locations;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dagger.android.support.DaggerFragment;
+import fr.free.nrw.commons.R;
+import fr.free.nrw.commons.contributions.ContributionController;
+import fr.free.nrw.commons.nearby.Place;
+import fr.free.nrw.commons.nearby.fragments.CommonPlaceClickActions;
+import fr.free.nrw.commons.nearby.fragments.PlaceAdapter;
+import java.util.List;
+import javax.inject.Inject;
+import kotlin.Unit;
+
+public class BookmarkLocationsFragment extends DaggerFragment {
+
+    @BindView(R.id.statusMessage) TextView statusTextView;
+    @BindView(R.id.loadingImagesProgressBar) ProgressBar progressBar;
+    @BindView(R.id.listView) RecyclerView recyclerView;
+    @BindView(R.id.parentLayout) RelativeLayout parentLayout;
+
+    @Inject BookmarkLocationsController controller;
+    @Inject ContributionController contributionController;
+    @Inject BookmarkLocationsDao bookmarkLocationDao;
+    @Inject CommonPlaceClickActions commonPlaceClickActions;
+    private PlaceAdapter adapter;
+
+    /**
+     * Create an instance of the fragment with the right bundle parameters
+     * @return an instance of the fragment
+     */
+    public static BookmarkLocationsFragment newInstance() {
+        return new BookmarkLocationsFragment();
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View v = inflater.inflate(R.layout.fragment_bookmarks_locations, container, false);
+        ButterKnife.bind(this, v);
+        return v;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        progressBar.setVisibility(View.VISIBLE);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new PlaceAdapter(bookmarkLocationDao,
+            place -> Unit.INSTANCE,
+            (place, isBookmarked) -> {
+                adapter.remove(place);
+                return Unit.INSTANCE;
+            },
+            commonPlaceClickActions
+        );
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        initList();
+    }
+
+    /**
+     * Initialize the recycler view with bookmarked locations
+     */
+    private void initList() {
+        List<Place> places = controller.loadFavoritesLocations();
+        adapter.setItems(places);
+        progressBar.setVisibility(View.GONE);
+        if (places.size() <= 0) {
+            statusTextView.setText(R.string.bookmark_empty);
+            statusTextView.setVisibility(View.VISIBLE);
+        } else {
+            statusTextView.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        contributionController.handleActivityResult(getActivity(), requestCode, resultCode, data);
+    }
+}
---
+++ category/CategoryClient.kt
@@ -0,0 +1,99 @@
+package fr.free.nrw.commons.category
+
+import io.reactivex.Single
+import org.wikipedia.dataclient.mwapi.MwQueryResponse
+import javax.inject.Inject
+import javax.inject.Singleton
+
+const val CATEGORY_PREFIX = "Category:"
+const val SUB_CATEGORY_CONTINUATION_PREFIX = "sub_category_"
+const val PARENT_CATEGORY_CONTINUATION_PREFIX = "parent_category_"
+
+/**
+ * Category Client to handle custom calls to Commons MediaWiki APIs
+ */
+@Singleton
+class CategoryClient @Inject constructor(private val categoryInterface: CategoryInterface) :
+    ContinuationClient<MwQueryResponse, String>() {
+
+    /**
+     * Searches for categories containing the specified string.
+     *
+     * @param filter    The string to be searched
+     * @param itemLimit How many results are returned
+     * @param offset    Starts returning items from the nth result. If offset is 9, the response starts with the 9th item of the search result
+     * @return
+     */
+    @JvmOverloads
+    fun searchCategories(filter: String?, itemLimit: Int, offset: Int = 0):
+            Single<List<String>> {
+        return responseMapper(categoryInterface.searchCategories(filter, itemLimit, offset))
+    }
+
+    /**
+     * Searches for categories starting with the specified string.
+     *
+     * @param prefix    The prefix to be searched
+     * @param itemLimit How many results are returned
+     * @param offset    Starts returning items from the nth result. If offset is 9, the response starts with the 9th item of the search result
+     * @return
+     */
+    @JvmOverloads
+    fun searchCategoriesForPrefix(prefix: String?, itemLimit: Int, offset: Int = 0):
+            Single<List<String>> {
+        return responseMapper(
+            categoryInterface.searchCategoriesForPrefix(prefix, itemLimit, offset)
+        )
+    }
+
+    /**
+     * The method takes categoryName as input and returns a List of Subcategories
+     * It uses the generator query API to get the subcategories in a category, 500 at a time.
+     *
+     * @param categoryName Category name as defined on commons
+     * @return Observable emitting the categories returned. If our search yielded "Category:Test", "Test" is emitted.
+     */
+    fun getSubCategoryList(categoryName: String): Single<List<String>> {
+        return continuationRequest(SUB_CATEGORY_CONTINUATION_PREFIX, categoryName) {
+            categoryInterface.getSubCategoryList(
+                categoryName, it
+            )
+        }
+    }
+
+    /**
+     * The method takes categoryName as input and returns a List of parent categories
+     * It uses the generator query API to get the parent categories of a category, 500 at a time.
+     *
+     * @param categoryName Category name as defined on commons
+     * @return
+     */
+    fun getParentCategoryList(categoryName: String): Single<List<String>> {
+        return continuationRequest(PARENT_CATEGORY_CONTINUATION_PREFIX, categoryName) {
+            categoryInterface.getParentCategoryList(categoryName, it)
+        }
+    }
+
+    fun resetSubCategoryContinuation(category: String) {
+        resetContinuation(SUB_CATEGORY_CONTINUATION_PREFIX, category)
+    }
+
+    fun resetParentCategoryContinuation(category: String) {
+        resetContinuation(PARENT_CATEGORY_CONTINUATION_PREFIX, category)
+    }
+
+    override fun responseMapper(
+        networkResult: Single<MwQueryResponse>,
+        key: String?
+    ): Single<List<String>> {
+        return networkResult
+            .map {
+                handleContinuationResponse(it.continuation(), key)
+                it.query()?.pages() ?: emptyList()
+            }
+            .map {
+                it.map { page -> page.title().replace(CATEGORY_PREFIX, "") }
+            }
+    }
+
+}
